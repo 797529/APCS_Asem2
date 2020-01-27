479344171cf36476e125e2c9ad8c14c7a1667207
797529 <54371358+797529@users.noreply.github.com>
2020-01-27 10:25:42 -0800

    more of this broken project done :,)

--- CelebStudentCode/SimpleGUIRunner.java
+++ CelebStudentCode/SimpleGUIRunner.java
@@ -1,11 +1,38 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class SimpleGUIRunner
 {
-	public static void main(String[] args)
-	{
-		JFrame sample = new JFrame();
-		sample.setVisible(true);
-	}
+    public static void main(String[] args)
+    {
+        //1. Create the frame.
+        JLabel yellowLabel = new JLabel();
+        yellowLabel.setOpaque(true);
+        yellowLabel.setBackground(new Color(255, 255, 0));
+        yellowLabel.setPreferredSize(new Dimension(200, 180));
 
+        JMenuBar topBar = new JMenuBar();
+
+        JMenu m2 = new JMenu();
+
+        topBar.add(m2);
+        
+        JFrame frame = new JFrame("Bruh");
+
+        frame.setJMenuBar(topBar);
+
+        //2. Optional: What happens when the frame closes?
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //3. Create components and put them in the frame.
+        //...create emptyLabel...
+        frame.getContentPane().add(yellowLabel, BorderLayout.CENTER);
+        frame.getContentPane().add(topBar, BorderLayout.NORTH);
+
+        //4. Size the frame.
+        frame.pack();
+
+        //5. Show it.
+        frame.setVisible(true);;
+    }
 }
