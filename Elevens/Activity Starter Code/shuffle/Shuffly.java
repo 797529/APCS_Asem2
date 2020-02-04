0c9ea4df9a2809e2ca3086c13b6321dea6a419ba
797529 <54371358+797529@users.noreply.github.com>
2020-02-04 10:26:44 -0800

    shuffling moment

---
+++ Elevens/Activity Starter Code/shuffle/Shuffly.java
@@ -0,0 +1,20 @@
+import java.util.*;
+/**
+ * Write a description of class Shuffly here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Shuffly
+{
+    public Shuffly()
+    {
+        
+    }
+    public static void main(){
+        ArrayList<Integer> shuff = new ArrayList<Integer>();
+        for(int i = 0; i < 52; i++){
+            shuff.add(i);
+        }
+    }
+}
